80e8d1c73c54028be50985be9fae46066a3a311b
turbo-lover <the.guard@mail.ru>
2013-12-21 12:27:51 +0400

    mess

--- sentinel.c
+++ sentinel.c
@@ -1,7 +1,90 @@
-//#include <ntddk.h>
-//#include <Wdm.h>
 #include <Fltkernel.h>
 #include <Bcrypt.h>
+#include <windef.h>
+
+/* workaround for an undefined type of an unsupported variable */
+typedef PVOID* PPS_POST_PROCESS_INIT_ROUTINE; 
+
+// the following structures are from msdn
+typedef struct _RTL_USER_PROCESS_PARAMETERS {
+  BYTE           Reserved1[16];
+  PVOID          Reserved2[10];
+  UNICODE_STRING ImagePathName;
+  UNICODE_STRING CommandLine;
+} RTL_USER_PROCESS_PARAMETERS, *PRTL_USER_PROCESS_PARAMETERS;
+/*
+typedef struct _LIST_ENTRY {
+  struct _LIST_ENTRY *Flink;
+  struct _LIST_ENTRY *Blink;
+} LIST_ENTRY, *PLIST_ENTRY, *RESTRICTED_POINTER PRLIST_ENTRY;
+*/
+typedef struct _LDR_DATA_TABLE_ENTRY {
+  PVOID Reserved1[2];
+  LIST_ENTRY InMemoryOrderLinks;
+  PVOID Reserved2[2];
+  PVOID DllBase;
+  PVOID EntryPoint;
+  PVOID Reserved3;
+  UNICODE_STRING FullDllName;
+  BYTE Reserved4[8];
+  PVOID Reserved5[3];
+  union {
+    ULONG CheckSum;
+    PVOID Reserved6;
+  };
+  ULONG TimeDateStamp;
+} LDR_DATA_TABLE_ENTRY, *PLDR_DATA_TABLE_ENTRY;
+typedef struct _LDR_DATA_TABLE_ENTRY32 {
+  DWORD Reserved1[2];
+  LIST_ENTRY InMemoryOrderLinks;
+  DWORD Reserved2[2];
+  DWORD DllBase;
+  DWORD EntryPoint;
+  DWORD Reserved3;
+  UNICODE_STRING FullDllName;
+  BYTE Reserved4[8];
+  PVOID Reserved5[3];
+  union {
+    ULONG CheckSum;
+    PVOID Reserved6;
+  };
+  ULONG TimeDateStamp;
+} LDR_DATA_TABLE_ENTRY32, *PLDR_DATA_TABLE_ENTRY32;
+typedef struct _PEB_LDR_DATA {
+  BYTE       Reserved1[8];
+  PVOID      Reserved2[3];
+  LIST_ENTRY InMemoryOrderModuleList;
+} PEB_LDR_DATA, *PPEB_LDR_DATA;
+/* for 32 bit windows */
+typedef struct _PEB32 {
+  BYTE                          Reserved1[2];
+  BYTE                          BeingDebugged;
+  BYTE                          Reserved2[1];
+  DWORD                         Reserved3[2];
+  DWORD                 Ldr;
+  DWORD  ProcessParameters;
+  BYTE                          Reserved4[104];
+  DWORD                         Reserved5[52];
+  DWORD PostProcessInitRoutine;
+  BYTE                          Reserved6[128];
+  DWORD                         Reserved7[1];
+  DWORD                         SessionId;
+} PEB32, *PPEB32;
+/* for 64 bit windows */
+typedef struct _PEB64 {
+  BYTE Reserved1[2];
+  BYTE BeingDebugged;
+  BYTE Reserved2[21];
+  PPEB_LDR_DATA LoaderData;
+  PRTL_USER_PROCESS_PARAMETERS ProcessParameters;
+  BYTE Reserved3[520];
+  PPS_POST_PROCESS_INIT_ROUTINE PostProcessInitRoutine;
+  BYTE Reserved4[136];
+  ULONG SessionId;
+} PEB64, *PPEB64;
+// this is from some forum 
+PPEB PsGetProcessPeb(PEPROCESS Process);
+
 
 #define IOCTL_SENTINEL_PPID CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800, METHOD_IN_DIRECT, FILE_ANY_ACCESS)
 
@@ -121,6 +204,11 @@ NTSTATUS HandleIOCTL(PDEVICE_OBJECT  DriverObject, PIRP Irp){
   PEPROCESS proc;
   KAPC_STATE apc_state;
   BCRYPT_HASH_HANDLE hHash;
+  PLIST_ENTRY Module, HeadModule;
+  PLDR_DATA_TABLE_ENTRY dte;
+  PEB64 *peb64;
+  PEB32 *peb32;
+  DWORD PTR32;
 
   DbgPrint("IOCTL handler called\r\n");
   switch (pIoStackIrp->
@@ -142,7 +230,7 @@ NTSTATUS HandleIOCTL(PDEVICE_OBJECT  DriverObject, PIRP Irp){
 	//supplied big enough
 	&&*/ Irp->AssociatedIrp.SystemBuffer //input buffer
 	&& (pIoStackIrp->
-	    Parameters.DeviceIoControl.OutputBufferLength >= 5*sizeof(int))
+	    Parameters.DeviceIoControl.OutputBufferLength >= 4*sizeof(int))
 	// supplied big enough
 	&& OutBuffer){ //outbuffer
       //attach, read, hash
@@ -150,14 +238,28 @@ NTSTATUS HandleIOCTL(PDEVICE_OBJECT  DriverObject, PIRP Irp){
       DbgPrint("attaching to pid %i\n", hri->pid);
       if(NT_SUCCESS(PsLookupProcessByProcessId(hri->pid, &proc))){
 	KeStackAttachProcess(proc, &apc_state);
-	BCryptCreateHash(hAlgo, &hHandle, );
-	BCryptHashData(hHandle, hri->start, hri->size, 0);
+	peb64 = PsGetProcessPeb(proc);
+	peb32 = (char *)peb64 - 0x1000;
+	HeadModule = &(peb64->LoaderData->InMemoryOrderModuleList);
+	for( Module = HeadModule->Flink;
+	     Module != HeadModule; Module = Module->Flink){
+	  dte = (PVOID *)Module - 2;
+	  DbgPrint("%wZ @ %p\n", dte->FullDllName, dte->DllBase);
+	}
+	PTR32 = peb32->Ldr;
+	HeadModule = &(((PPEB_LDR_DATA)PTR32)->InMemoryOrderModuleList);
+	for( PTR32 = HeadModule->Flink;
+	     PTR32 != HeadModule; PTR32 = ((PLIST_ENTRY)PTR32)->Flink){
+	  dte = PTR32 - 2;
+	  DbgPrint("%wZ @ %p\n", dte->FullDllName, dte->DllBase);
+	}
 	
 	KeUnstackDetachProcess(&apc_state);
 	ObDereferenceObject(proc);
 	status = STATUS_SUCCESS;
       }
     }
+    else DbgPrint("Argument mismatch");
     break;
   default:
     status = STATUS_INVALID_DEVICE_REQUEST;
