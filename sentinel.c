ef9b6b6bf3d0ea0996f243c4c6804a73ebb0cb4b
turbo-lover <the.guard@mail.ru>
2013-12-24 14:13:14 +0400

    added hashing

--- sentinel.c
+++ sentinel.c
@@ -1,273 +1,5 @@
-#include <Fltkernel.h>
-#include <Bcrypt.h>
-#include <windef.h>
-
-/* workaround for an undefined type of an unsupported variable */
-typedef PVOID* PPS_POST_PROCESS_INIT_ROUTINE; 
-
-// the following structures are from msdn
-typedef struct _RTL_USER_PROCESS_PARAMETERS {
-  BYTE           Reserved1[16];
-  PVOID          Reserved2[10];
-  UNICODE_STRING ImagePathName;
-  UNICODE_STRING CommandLine;
-} RTL_USER_PROCESS_PARAMETERS, *PRTL_USER_PROCESS_PARAMETERS;
-/*
-typedef struct _LIST_ENTRY {
-  struct _LIST_ENTRY *Flink;
-  struct _LIST_ENTRY *Blink;
-} LIST_ENTRY, *PLIST_ENTRY, *RESTRICTED_POINTER PRLIST_ENTRY;
-*/
-typedef struct _LDR_DATA_TABLE_ENTRY {
-  PVOID Reserved1[2];
-  LIST_ENTRY InMemoryOrderLinks;
-  PVOID Reserved2[2];
-  PVOID DllBase;
-  PVOID EntryPoint;
-  PVOID Reserved3;
-  UNICODE_STRING FullDllName;
-  BYTE Reserved4[8];
-  PVOID Reserved5[3];
-  union {
-    ULONG CheckSum;
-    PVOID Reserved6;
-  };
-  ULONG TimeDateStamp;
-} LDR_DATA_TABLE_ENTRY, *PLDR_DATA_TABLE_ENTRY;
-/*
-typedef struct _LSA_UNICODE_STRING32 {
-  USHORT Length;
-  USHORT MaximumLength;
-  DWORD  Buffer;
-} LSA_UNICODE_STRING32, *PLSA_UNICODE_STRING32, UNICODE_STRING32, *PUNICODE_STRING32;
-typedef struct _LIST_ENTRY32 {
-  DWORD Flink;
-  DWORD Blink;
-} LIST_ENTRY32, *PLIST_ENTRY32;
-*/
-typedef struct _LDR_DATA_TABLE_ENTRY32 {
-  DWORD Reserved1[2];
-  LIST_ENTRY32 InMemoryOrderLinks;
-  DWORD Reserved2[2];
-  DWORD DllBase;
-  DWORD EntryPoint;
-  DWORD Reserved3;
-  UNICODE_STRING32 FullDllName;
-  BYTE Reserved4[8];
-  DWORD Reserved5[3];
-  union {
-    DWORD CheckSum;
-    DWORD Reserved6;
-  };
-  DWORD TimeDateStamp;
-} LDR_DATA_TABLE_ENTRY32, *PLDR_DATA_TABLE_ENTRY32;
-typedef struct _PEB_LDR_DATA32 {
-  BYTE       Reserved1[8];
-  DWORD      Reserved2[3];
-  LIST_ENTRY32 InMemoryOrderModuleList;
-} PEB_LDR_DATA32, *PPEB_LDR_DATA32;
-typedef struct _PEB_LDR_DATA {
-  BYTE       Reserved1[8];
-  PVOID      Reserved2[3];
-  LIST_ENTRY InMemoryOrderModuleList;
-} PEB_LDR_DATA, *PPEB_LDR_DATA;
-/* for 32 bit windows */
-typedef struct _PEB32 {
-  BYTE                          Reserved1[2];
-  BYTE                          BeingDebugged;
-  BYTE                          Reserved2[1];
-  DWORD                         Reserved3[2];
-  DWORD                 Ldr;
-  DWORD  ProcessParameters;
-  BYTE                          Reserved4[104];
-  DWORD                         Reserved5[52];
-  DWORD PostProcessInitRoutine;
-  BYTE                          Reserved6[128];
-  DWORD                         Reserved7[1];
-  DWORD                         SessionId;
-} PEB32, *PPEB32;
-/* for 64 bit windows */
-typedef struct _PEB64 {
-  BYTE Reserved1[2];
-  BYTE BeingDebugged;
-  BYTE Reserved2[21];
-  PPEB_LDR_DATA LoaderData;
-  PRTL_USER_PROCESS_PARAMETERS ProcessParameters;
-  BYTE Reserved3[520];
-  PPS_POST_PROCESS_INIT_ROUTINE PostProcessInitRoutine;
-  BYTE Reserved4[136];
-  ULONG SessionId;
-} PEB64, *PPEB64;
-// this is from some forum 
-PPEB PsGetProcessPeb(PEPROCESS Process);
-typedef struct _IMAGE_DOS_HEADER {      // DOS .EXE header
-    WORD   e_magic;                     // Magic number
-    WORD   e_cblp;                      // Bytes on last page of file
-    WORD   e_cp;                        // Pages in file
-    WORD   e_crlc;                      // Relocations
-    WORD   e_cparhdr;                   // Size of header in paragraphs
-    WORD   e_minalloc;                  // Minimum extra paragraphs needed
-    WORD   e_maxalloc;                  // Maximum extra paragraphs needed
-    WORD   e_ss;                        // Initial (relative) SS value
-    WORD   e_sp;                        // Initial SP value
-    WORD   e_csum;                      // Checksum
-    WORD   e_ip;                        // Initial IP value
-    WORD   e_cs;                        // Initial (relative) CS value
-    WORD   e_lfarlc;                    // File address of relocation table
-    WORD   e_ovno;                      // Overlay number
-    WORD   e_res[4];                    // Reserved words
-    WORD   e_oemid;                     // OEM identifier (for e_oeminfo)
-    WORD   e_oeminfo;                   // OEM information; e_oemid specific
-    WORD   e_res2[10];                  // Reserved words
-    LONG   e_lfanew;                    // File address of new exe header
-} IMAGE_DOS_HEADER, *PIMAGE_DOS_HEADER;
-typedef struct _IMAGE_FILE_HEADER {
-    WORD    Machine;
-    WORD    NumberOfSections;
-    DWORD   TimeDateStamp;
-    DWORD   PointerToSymbolTable;
-    DWORD   NumberOfSymbols;
-    WORD    SizeOfOptionalHeader;
-    WORD    Characteristics;
-} IMAGE_FILE_HEADER, *PIMAGE_FILE_HEADER;
-typedef struct _IMAGE_DATA_DIRECTORY {
-    DWORD   VirtualAddress;
-    DWORD   Size;
-} IMAGE_DATA_DIRECTORY, *PIMAGE_DATA_DIRECTORY;
-#define IMAGE_NUMBEROF_DIRECTORY_ENTRIES    16
-typedef struct _IMAGE_OPTIONAL_HEADER {
-    //
-    // Standard fields.
-    //
-
-    WORD    Magic;
-    BYTE    MajorLinkerVersion;
-    BYTE    MinorLinkerVersion;
-    DWORD   SizeOfCode;
-    DWORD   SizeOfInitializedData;
-    DWORD   SizeOfUninitializedData;
-    DWORD   AddressOfEntryPoint;
-    DWORD   BaseOfCode;
-    DWORD   BaseOfData;
-
-    //
-    // NT additional fields.
-    //
-
-    DWORD   ImageBase;
-    DWORD   SectionAlignment;
-    DWORD   FileAlignment;
-    WORD    MajorOperatingSystemVersion;
-    WORD    MinorOperatingSystemVersion;
-    WORD    MajorImageVersion;
-    WORD    MinorImageVersion;
-    WORD    MajorSubsystemVersion;
-    WORD    MinorSubsystemVersion;
-    DWORD   Win32VersionValue;
-    DWORD   SizeOfImage;
-    DWORD   SizeOfHeaders;
-    DWORD   CheckSum;
-    WORD    Subsystem;
-    WORD    DllCharacteristics;
-    DWORD   SizeOfStackReserve;
-    DWORD   SizeOfStackCommit;
-    DWORD   SizeOfHeapReserve;
-    DWORD   SizeOfHeapCommit;
-    DWORD   LoaderFlags;
-    DWORD   NumberOfRvaAndSizes;
-    IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
-} IMAGE_OPTIONAL_HEADER32, *PIMAGE_OPTIONAL_HEADER32;
-typedef struct _IMAGE_OPTIONAL_HEADER64 {
-    WORD        Magic;
-    BYTE        MajorLinkerVersion;
-    BYTE        MinorLinkerVersion;
-    DWORD       SizeOfCode;
-    DWORD       SizeOfInitializedData;
-    DWORD       SizeOfUninitializedData;
-    DWORD       AddressOfEntryPoint;
-    DWORD       BaseOfCode;
-    ULONGLONG   ImageBase;
-    DWORD       SectionAlignment;
-    DWORD       FileAlignment;
-    WORD        MajorOperatingSystemVersion;
-    WORD        MinorOperatingSystemVersion;
-    WORD        MajorImageVersion;
-    WORD        MinorImageVersion;
-    WORD        MajorSubsystemVersion;
-    WORD        MinorSubsystemVersion;
-    DWORD       Win32VersionValue;
-    DWORD       SizeOfImage;
-    DWORD       SizeOfHeaders;
-    DWORD       CheckSum;
-    WORD        Subsystem;
-    WORD        DllCharacteristics;
-    ULONGLONG   SizeOfStackReserve;
-    ULONGLONG   SizeOfStackCommit;
-    ULONGLONG   SizeOfHeapReserve;
-    ULONGLONG   SizeOfHeapCommit;
-    DWORD       LoaderFlags;
-    DWORD       NumberOfRvaAndSizes;
-    IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
-} IMAGE_OPTIONAL_HEADER64, *PIMAGE_OPTIONAL_HEADER64;
-typedef struct _IMAGE_NT_HEADERS64 {
-    DWORD Signature;
-    IMAGE_FILE_HEADER FileHeader;
-    IMAGE_OPTIONAL_HEADER64 OptionalHeader;
-} IMAGE_NT_HEADERS64, *PIMAGE_NT_HEADERS64;
-
-typedef struct _IMAGE_NT_HEADERS {
-    DWORD Signature;
-    IMAGE_FILE_HEADER FileHeader;
-    IMAGE_OPTIONAL_HEADER32 OptionalHeader;
-} IMAGE_NT_HEADERS32, *PIMAGE_NT_HEADERS32;
-#define IMAGE_SIZEOF_SHORT_NAME              8
-typedef struct _IMAGE_SECTION_HEADER {
-    BYTE    Name[IMAGE_SIZEOF_SHORT_NAME];
-    union {
-            DWORD   PhysicalAddress;
-            DWORD   VirtualSize;
-    } Misc;
-    DWORD   VirtualAddress;
-    DWORD   SizeOfRawData;
-    DWORD   PointerToRawData;
-    DWORD   PointerToRelocations;
-    DWORD   PointerToLinenumbers;
-    WORD    NumberOfRelocations;
-    WORD    NumberOfLinenumbers;
-    DWORD   Characteristics;
-} IMAGE_SECTION_HEADER, *PIMAGE_SECTION_HEADER;
-#define IMAGE_SCN_CNT_CODE                   0x00000020  // Section contains code.
-#define IMAGE_SCN_CNT_INITIALIZED_DATA       0x00000040  // Section contains initialized data.
-#define IMAGE_SCN_MEM_EXECUTE                0x20000000  // Section is executable.
-#define IMAGE_SCN_MEM_READ                   0x40000000  // Section is readable.
-#define IMAGE_SCN_MEM_WRITE                  0x80000000  // Section is writeable.
-
-
-#define IOCTL_SENTINEL_PPID CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800, METHOD_IN_DIRECT, FILE_ANY_ACCESS)
-
-#define IOCTL_SENTINEL_HASH CTL_CODE(FILE_DEVICE_UNKNOWN, 0x801, METHOD_IN_DIRECT, FILE_ANY_ACCESS)
-
-int RegistrationHandle;
-//int CBCKRegistered;
-int *pidlist = NULL;
-
-typedef struct{
-  int pid; // long???
-  PVOID start;
-  int size;
-}  HashReqData;
-
-NTSTATUS Read(PDEVICE_OBJECT, PIRP);
-NTSTATUS Create(PDEVICE_OBJECT, PIRP);
-NTSTATUS Close(PDEVICE_OBJECT, PIRP);
-NTSTATUS HandleIOCTL(PDEVICE_OBJECT, PIRP);
-NTSTATUS NotImplemented(PDEVICE_OBJECT, PIRP);
-void Dtor(PDRIVER_OBJECT );
-OB_PREOP_CALLBACK_STATUS PreCallback(PVOID, POB_PRE_OPERATION_INFORMATION);
-void PostCallback(PVOID, POB_POST_OPERATION_INFORMATION);
-NTSTATUS SHA1(PVOID data, int size, int *hash){
-  BCRYPT_ALG_HANDLE halg;
-}
+#include "sentinel.h"
+#include "sha1.h"
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath){
   NTSTATUS NtStatus = STATUS_SUCCESS;
@@ -364,7 +96,6 @@ NTSTATUS HandleIOCTL(PDEVICE_OBJECT  DriverObject, PIRP Irp){
   HashReqData *hri;
   PEPROCESS proc;
   KAPC_STATE apc_state;
-  BCRYPT_HASH_HANDLE hHash;
   PLIST_ENTRY Module, HeadModule;
   PLDR_DATA_TABLE_ENTRY dte;
   PEB64 *peb64;
@@ -375,6 +106,7 @@ NTSTATUS HandleIOCTL(PDEVICE_OBJECT  DriverObject, PIRP Irp){
   PIMAGE_NT_HEADERS32 nth32;
   PIMAGE_NT_HEADERS64 nth64;
   PIMAGE_SECTION_HEADER ish;
+  SHA1Context sha;
 
   DbgPrint("IOCTL handler called\r\n");
   switch (pIoStackIrp->
@@ -417,8 +149,20 @@ NTSTATUS HandleIOCTL(PDEVICE_OBJECT  DriverObject, PIRP Irp){
 	  ish = (char *) &(nth64->OptionalHeader) 
 	    + nth64->FileHeader.SizeOfOptionalHeader;
 	  for( i = 0; i < nsec; i++){
-	    DbgPrint("%p\n", ish[i].VirtualAddress + (char *)dosh);
-	    //ish[i].Misc.VirtualSize
+	    if(ish[i].Characteristics & IMAGE_SCN_CNT_CODE){
+	      DbgPrint("%p w/ sha1: ", ish[i].VirtualAddress + (char *)dosh);
+	      SHA1Reset(&sha);
+	      SHA1Input(&sha, 
+			(PUCHAR *) (ish[i].VirtualAddress + (char *)dosh), 
+			ish[i].Misc.VirtualSize);
+	      if(SHA1Result(&sha))
+		DbgPrint("%X%X%X%X%X", sha.Message_Digest[0],
+			 sha.Message_Digest[1],
+			 sha.Message_Digest[2],
+			 sha.Message_Digest[3],
+			 sha.Message_Digest[4]);
+		      
+	    }
 	  }
 	}
 	PTR32 = peb32->Ldr;
@@ -439,10 +183,22 @@ NTSTATUS HandleIOCTL(PDEVICE_OBJECT  DriverObject, PIRP Irp){
 	  ish = (char *) &(nth32->OptionalHeader) 
 	    + nth32->FileHeader.SizeOfOptionalHeader;
 	  for( i = 0; i < nsec; i++){
-	    DbgPrint("%p\n", ish[i].VirtualAddress + (char *)dosh);
+	    if(ish[i].Characteristics & IMAGE_SCN_CNT_CODE){
+	      DbgPrint("%p w/ sha1: ", ish[i].VirtualAddress + (char *)dosh);
+	      SHA1Reset(&sha);
+	      SHA1Input(&sha, 
+			(PUCHAR *) (ish[i].VirtualAddress + (char *)dosh), 
+			ish[i].Misc.VirtualSize);
+	      if(SHA1Result(&sha))
+		DbgPrint("%X%X%X%X%X", sha.Message_Digest[0],
+			 sha.Message_Digest[1],
+			 sha.Message_Digest[2],
+			 sha.Message_Digest[3],
+			 sha.Message_Digest[4]);
+	    }
 	  }
 	}
-      
+	
 	KeUnstackDetachProcess(&apc_state);
 	ObDereferenceObject(proc);
 	status = STATUS_SUCCESS;
